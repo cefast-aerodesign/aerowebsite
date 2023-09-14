9809bbd3559c7c9980e9f97d72f24badf7b4964b
Gabriel Lamarth <glcoodex@gmail.com>
2023-09-14 18:42:15 -0300

    adiciona códigos de cores

--- src/shared/colors.ts
+++ src/shared/colors.ts
@@ -1,7 +1,17 @@
  const colors = {
-    'white': '#f9f9f9',
-    'orange': '#FE5600',
-    'black': '#ffff',
+    'white': '#FFFFFF',
+    'pale_orange': '#FCD2BD',
+    'orange_common': '#FE5600',
+    'orange_alternative': '#FF5517',
+    'orange_gradient': '#683020', 
+    'black': '#000000',
+    'pale_gray': '#F9F9F9',
+    'gray_light': '#3F3E3E',
+    'gray_medium': '#252424',
+    'gray_dark': '#181818',
+    'blue_dark': '#00386A',
+    'silver': '#D9D9D9', 
+
 }
 
 export default colors
