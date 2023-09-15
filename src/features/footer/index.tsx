3b94787db4307d1dbe3131dba9662cb727ba30ca
hray-0 <glcoodex@gmail.com>
2023-09-15 17:29:10 -0300

    instalação de pacotes e estrutura básica do arquivo footer

---
+++ src/features/footer/index.tsx
@@ -0,0 +1,19 @@
+/////// footer
+
+// import sec
+import React from 'react';
+
+// extra functions sec
+
+// main sec
+const Footer = () => {
+    return (
+        <div>  
+            <footer>
+                <header>UUH</header>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
