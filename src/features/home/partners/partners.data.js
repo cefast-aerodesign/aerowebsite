095628bab92645bc3960c50390d36d1544dbae34
cristian-oliveira137 <cristianlso137@gmail.com>
2023-09-21 17:20:58 -0300

    Lista de parceiros

---
+++ src/features/home/partners/partners.data.js
@@ -0,0 +1,10 @@
+
+const partnersList = [
+    {
+        id: 0,
+        name: 'LEO MADEIRAS',
+        partnerLevel: 'gold',
+        urlPage: 'https://www.leomadeiras.com.br/',
+        image: leoMadeiras
+    },
+]
