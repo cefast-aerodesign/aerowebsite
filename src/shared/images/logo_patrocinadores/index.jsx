cb7976fccb0e12bdc41f450748f3a0de89c07163
cristian-oliveira137 <cristianlso137@gmail.com>
2023-09-21 17:09:38 -0300

    Lista de patrocinadores

--- src/shared/images/logo_patrocinadores/index.jsx
+++ src/shared/images/logo_patrocinadores/index.jsx
@@ -1,5 +1,6 @@
 import cefet from "./cefet-mg.svg";
 import cefast from "./cefast-logo.svg";
+import leoMadeiras from "./leo_leo.svg";
 import bannerHeaderHome from "./banner-header-home.svg";
 
 export { cefet, cefast , bannerHeaderHome};
