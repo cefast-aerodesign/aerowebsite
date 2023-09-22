bff2f426760638c6e10e94903008a1b801a729ae
cristian-oliveira137 <cristianlso137@gmail.com>
2023-09-22 15:28:24 -0300

    Redirects

--- src/features/footer/index.jsx
+++ src/features/footer/index.jsx
@@ -6,19 +6,30 @@ import { cefast, cefet } from "../../shared/images";
 import SocialNetworkIcons from "./socialNetworks";
 
 const Footer = () => {
+  const contactData = {
+    primaryEmail: "cefastaeroadm@gmail.com",
+    secondaryEmail: "cefastaeromkt@gmail.com",
+    tel: "+55 31 98794-3331",
+  };
   return (
     <FooterContainer>
       <Grid container alignItems="center" direction="row" spacing={9}>
         <Grid item xs>
           <Grid container direction="column" spacing={2}>
             <Grid item xs>
-              <P1>cefastaeroadm@gmail.com</P1>
+              <a className="redirect" href={contactData.primaryEmail}>
+                <P1>{contactData.primaryEmail}</P1>
+              </a>
             </Grid>
             <Grid item xs>
-              <P1>cefastaeromkt@gmail.com</P1>
+              <a className="redirect" href={contactData.secondaryEmail}>
+                <P1>{contactData.secondaryEmail}</P1>
+              </a>
             </Grid>
             <Grid item xs>
-              <P1>+55 31 98794-3331</P1>
+              <a className="redirect" href={'tel:' + contactData.tel}>
+                <P1>{contactData.tel}</P1>
+              </a>
             </Grid>
             <Grid item xs>
               <Grid container spacing={4} direction="rows">
@@ -36,25 +47,28 @@ const Footer = () => {
           </a>
         </Grid>
         <Grid item xs>
-          <Grid
-            container
-            direction="column"
-            justifyContent="flex-end"
-            className="map-container"
-            spacing={2}
+          <a
+            className="redirect"
+            href="https://www.google.com.br/maps/place/CEFET-MG+-+Campus+Nova+Gameleira/@-19.9387516,-44.0013638,766m/data=!3m2!1e3!4b1!4m6!3m5!1s0xa6973f497f73a5:0x72e85b0a73d210a1!8m2!3d-19.9387567!4d-43.9987835!16s%2Fg%2F11rdznbr_n?entry=ttu"
           >
-            <Grid item xs>
-              <P1>
-                Av. Amazonas, 7675 - Nova Gameleira, Belo Horizonte - MG,
-                30510-000
-              </P1>
-            </Grid>
-            <Grid item xs={12}>
-              <a href="https://www.google.com.br/maps/place/CEFET-MG+-+Campus+Nova+Gameleira/@-19.9387516,-44.0013638,766m/data=!3m2!1e3!4b1!4m6!3m5!1s0xa6973f497f73a5:0x72e85b0a73d210a1!8m2!3d-19.9387567!4d-43.9987835!16s%2Fg%2F11rdznbr_n?entry=ttu">
+            <Grid
+              container
+              direction="column"
+              justifyContent="flex-end"
+              className="map-container"
+              spacing={2}
+            >
+              <Grid item xs>
+                <P1 className="redirect">
+                  Av. Amazonas, 7675 - Nova Gameleira, Belo Horizonte - MG,
+                  30510-000
+                </P1>
+              </Grid>
+              <Grid item xs={12}>
                 <SeeMapButton variant="contained">Ver no mapa</SeeMapButton>
-              </a>
+              </Grid>
             </Grid>
-          </Grid>
+          </a>
         </Grid>
       </Grid>
     </FooterContainer>
--- src/features/footer/style.js
+++ src/features/footer/style.js
@@ -1,32 +1,36 @@
-import styled from '@emotion/styled';
-import colors from '../../shared/colors'
-import Button from "@mui/material/Button"
+import styled from "@emotion/styled";
+import colors from "../../shared/colors";
+import Button from "@mui/material/Button";
 
 const FooterContainer = styled.div`
-    background-color: ${colors.black};
-    padding: 16px 72px; 
-    height: 100%;
-    margin: 0;
+  background-color: ${colors.black};
+  padding: 16px 72px;
+  height: 100%;
+  margin: 0;
+  color: ${colors.pale_gray};
+  .map-container {
+    text-align: end;
+  }
+  .redirect {
     color: ${colors.pale_gray};
-    .map-container{
-        text-align: end;
-    }
+    text-decoration: none;
+  }
 `;
 
 const SeeMapButton = styled(Button)`
-    color: ${colors.gray_dark};
-    text-transform: capitalize;
-    min-width: 180px;
-    background-color: ${colors.pale_gray};    
-    .MuiButtonBase-root-MuiButton-root:hover {
-        background-color: ${colors.gray_medium};    
-    }
+  color: ${colors.gray_dark};
+  text-transform: capitalize;
+  min-width: 180px;
+  background-color: ${colors.pale_gray};
+  .MuiButtonBase-root-MuiButton-root:hover {
+    background-color: ${colors.gray_medium};
+  }
 `;
 
 const socialIcon = styled.div`
-    color: ${colors.pale_gray};
-    padding-right: 2px;
-    padding-left: 2px;
+  color: ${colors.pale_gray};
+  padding-right: 2px;
+  padding-left: 2px;
 `;
 
-export { FooterContainer, SeeMapButton, socialIcon }
+export { FooterContainer, SeeMapButton, socialIcon };
