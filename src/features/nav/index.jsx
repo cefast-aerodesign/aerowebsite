50de66faa09feb7dbe1e34247fd0a3f75632182e
cristian-oliveira137 <cristianlso137@gmail.com>
2023-10-31 14:59:19 -0300

    BUG FIX MAIN 2

--- src/features/nav/index.jsx
+++ src/features/nav/index.jsx
@@ -6,44 +6,38 @@ import { navList } from "../../shared/enums";
 import { P2 } from "../../shared/fonts";
 
 const Nav = ({ isScrolled, isDownCard, redirectTo }) => (
-    <NavContainer
-      className="nav-fixed"
-      $isScrolled={isScrolled}
-      $isDownCard={isDownCard}
-    >
-      <Grid container justifyContent="space-around" alignItems="center">
-        <Grid item xs={6}>
-          <a onClick={()=> redirectTo('/')}>
-            <img
-              src={cefastTextLogo}
-              width={160}
-              height={60}
-              alt="logo cefast aerodesign"
-            />
-          </a>
-        </Grid>
-        <Grid item xs>
-          <Grid
-            container
-            justifyContent="center"
-            spacing={8}
-            alignItems="center"
-          >
-            {navList.map((item) => (
-              <Grid item xs="auto" key={item.text}>
-                <a
-                  onClick={() => redirectTo(item.idRedirect)}
-                  className="nav-text"
-                >
-                  <P2>{item.text}</P2>
-                </a>
-              </Grid>
-            ))}
-          </Grid>
+  <NavContainer
+    className="nav-fixed"
+    $isScrolled={isScrolled}
+    $isDownCard={isDownCard}
+  >
+    <Grid container justifyContent="space-around" alignItems="center">
+      <Grid item xs={6}>
+        <a href="/">
+          <img
+            src={cefastTextLogo}
+            width={160}
+            height={60}
+            alt="logo cefast aerodesign"
+          />
+        </a>
+      </Grid>
+      <Grid item xs>
+        <Grid container justifyContent="center" spacing={8} alignItems="center">
+          {navList.map((item) => (
+            <Grid item xs="auto" key={item.text}>
+              <a
+                onClick={() => redirectTo(item.idRedirect)}
+                className="nav-text"
+              >
+                <P2>{item.text}</P2>
+              </a>
+            </Grid>
+          ))}
         </Grid>
       </Grid>
-    </NavContainer>
-  );
-
+    </Grid>
+  </NavContainer>
+);
 
 export default Nav;
